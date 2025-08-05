8511baa7eae7bb30c32fc924eca750bd7e97f457
Alaaeldeen Hamed <mregydev@gmail.com>
2025-08-06 00:34:23 +0300

    fix: remove speedinsights as its not working with vite

--- src/main.tsx
+++ src/main.tsx
@@ -8,14 +8,12 @@ import {
 import './index.css'; // your Tailwind or global styles
 import { Toaster } from 'sonner';
 import { queryClient } from './lib/queryClient';
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <Toaster />
       <App />
-      <SpeedInsights />
     </QueryClientProvider>
   </React.StrictMode>
 );
